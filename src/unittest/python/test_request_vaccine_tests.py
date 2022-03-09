53759dbb83a6691dcc3cef733bee756b048d64b1
LorenzoLargacha <100432129@alumnos.uc3m.es>
2022-03-09 22:50:24 +0100

    Creación del repositorio y configuración del entorno.

---
+++ src/unittest/python/test_request_vaccine_tests.py
@@ -0,0 +1,11 @@
+import unittest
+from uc3m_care import VaccineManager
+
+class MyTestCase(unittest.TestCase):
+    def test_something( self ):
+        #self.assertEqual(True, False)
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
